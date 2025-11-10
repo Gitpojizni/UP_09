c04a5e45c0fd332afeb12839fa420f9b0507b2fb
Gitpojizni <69abobu228@gmail.com>
2025-11-10 13:10:51 +0900

    можно завпускать на open server + регистрация

---
+++ sacroBlu/reg.php
@@ -0,0 +1,35 @@
+<?php 
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "AlikovDemo_1G";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$FIO = $_POST['FIO'];
+	
+    $login = $_POST['login'];
+	list($last_name, $name, $father_name) = explode(' ', $FIO); 
+	
+	$phone_num = $_POST['phone_num'];
+	$mail = $_POST['mail'];
+	$pass1 = $_POST['pass1'];
+	$pass2 = $_POST['pass2'];
+	
+	if ($pass1 == $pass2){
+		$query = "INSERT INTO `reg` (`login`, `name`, `last_name`, `father_name`, `phone`, `mail`, `password`, `status`)
+		VALUES ('$login', '$name', '$last_name', '$father_name', '$phone_num', '$mail', '$pass1', '0')";
+		
+		$result = mysqli_query($conn, $query);
+	} else {
+		echo "Пароли не совпадают";
+	}
+
+    file_put_contents('app.log', "Обработано: $name\n", FILE_APPEND);
+}
+readfile("see_bid.html");
+
+exit();
+?>
